6ba3b3b48ab0cf7a2af1564f907f52f49ea8f3ba
joanafm81 <114578901+joanafm81@users.noreply.github.com>
2023-07-17 23:04:12 +0100

    Update settings.py

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-ml49cp(e)=yakpe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['mysterious-coast-04552-eb2b3992692a.herokuapp.com']
 
 
 # Application definition
