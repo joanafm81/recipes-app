25bd9c73c40731d480776070c27e3658d9281ba8
joanafm81 <114578901+joanafm81@users.noreply.github.com>
2023-07-18 15:07:52 +0100

    Update settings.py

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -131,7 +131,7 @@ STATICFILES_DIRS=[
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Media files
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 MEDIA_ROOT= BASE_DIR / 'media'
 
 # Default primary key field type
